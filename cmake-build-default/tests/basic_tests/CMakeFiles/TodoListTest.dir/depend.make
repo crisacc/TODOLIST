00b719d95a0e150d5d64f313e306d3a3a9559597
crisacc <103118682+crisacc@users.noreply.github.com>
2025-03-25 15:10:15 +0100

    Delete cmake-build-default directory

--- cmake-build-default/tests/basic_tests/CMakeFiles/TodoListTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TodoListTest.
-# This may be replaced when dependencies are built.
--- cmake-build-default/tests/lib/googletest-1.15.2/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Cristiano/CLionProjects/TODOLIST/tests/lib/googletest-1.15.2/googletest
-# Build directory: C:/Users/Cristiano/CLionProjects/TODOLIST/cmake-build-default/tests/lib/googletest-1.15.2/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
