f5eb1a891e64c828bf5486b91e66926ea3198987
crisacc <103118682+crisacc@users.noreply.github.com>
2025-03-25 15:10:49 +0100

    Delete cmake-build-release directory

--- cmake-build-release/tests/lib/googletest-1.15.2/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Cristiano/CLionProjects/TODOLIST/tests/lib/googletest-1.15.2
-# Build directory: C:/Users/Cristiano/CLionProjects/TODOLIST/cmake-build-release/tests/lib/googletest-1.15.2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
